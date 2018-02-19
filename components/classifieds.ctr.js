58b70ccc95a999890e01475f4f82d7987e98cceb
Smriti <simmybajaj1603@gmail.com>
2018-02-19 08:56:19 +0530

    third commit

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,43 +1,89 @@
-(function(){
+(function () {
 
-angular
-.module("ngClassifieds")
-.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
-    classifiedsFactory.getClassifieds().then(function(classifieds) {
-        $scope.classifieds = classifieds.data;//  $scope.classifieds is an array
-    });// factoryname.function.work
+    angular
+        .module("ngClassifieds")
+        .controller("classifiedsCtrl", function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
+            classifiedsFactory.getClassifieds().then(function (classifieds) {
+                $scope.classifieds = classifieds.data;//  $scope.classifieds is an array
+                $scope.categories = getCategories($scope.classifieds);
+            });// factoryname.function.work
 
-    var contact={
-        name: "Simmy Bajaj",
-        phone: "(995) 343 8126",
-        email: "simmy@gmail.com"
-    }
-    $scope.openSidebar= function() {
-    $mdSidenav('left').open();
-    //open function
-    }
-    $scope.closeSidebar= function() {
-    $mdSidenav('left').close();
-    //close function
-    }
-    $scope.saveClassified = function(classified) {
-       if(classified)
-        { classified.contact= contact;
-            $scope.classifieds.push(classified);
-            $scope.classified={};// empty object feild
-            $scope.closeSidebar();// close navbar
-            $mdTosst.show(
-            $mdToast.simple()
-            .content("Classified Saved")
-            .position('top , right')
-            .hideDelay(3000)
-            )
-        }
-    }
-    $scope.editClassified = function(classified) {
-        $scope.editing = true;
-        $scope.openSidebar();
-        $scope.classified = classified;
-    }
-});
+            var contact = {
+                name: "Simmy Bajaj",
+                phone: "(995) 343 8126",
+                email: "simmy@gmail.com"
+            }
+
+            //$scope.categories = getCategories($scope.classifieds);
+
+            $scope.openSidebar = function () {
+                $mdSidenav('left').open();
+                //open function
+            }
+            $scope.closeSidebar = function () {
+                $mdSidenav('left').close();
+                //close function
+            }
+            $scope.saveClassified = function (classified) {
+                if (classified) {
+                classified.contact = contact;
+                    $scope.classifieds.push(classified);
+                    $scope.classified = {};// empty object feild
+                    $scope.closeSidebar();// close navbar
+                    showToast("Classified saved");
+                }
+            }
+
+            $scope.editClassified = function (classified) {
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.classified = classified;
+
+            }
+
+            $scope.saveEdit = function (classified) {
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSidebar();
+                showToast("Edited successfully");
+            }
+
+            // we will do splice to delete from frontend ie screen
+            $scope.deleteClassified = function (event, classified) {
+                var confirm = $mdDialog.confirm()
+                    .title('Are you sure you want to delete' + classified.title + '?')
+                    .ok('yes')
+                    .cancel('no')
+                    .targetEvent('event');
+                $mdDialog.show(confirm).then(function () {// promise is there by using then
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index, 1)
+                }, function () {
+
+                })
+
+            }
+
+            function showToast(message) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content(message)
+                        .position('top , right')
+                        .hideDelay(3000)
+                )
+            }
+            // use of lowdash to sort array
+            function getCategories(classifieds) {
+
+                var categories = [];
+
+                angular.forEach(classifieds, function (item) {
+                    angular.forEach(item.categories, function (category) {
+                        categories.push(category);
+                    });
+                });
+
+                return _.uniq(categories);
+            }
+        });
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,24 @@
 angular
-.module("ngClassifieds",["ngMaterial"])
-.config(function($mdThemingProvider) {
+.module("ngClassifieds",["ngMaterial", "ui.router"])
+.config(function($mdThemingProvider, $stateProvider) {
 
     $mdThemingProvider.theme('default')
     .primaryPalette('teal')
     .accentPalette('orange');
+
+    $stateProvider
+    .state('stateone',{
+        url: '/stateone',
+        template: '<h1>State one</h1>'
+    })
+    .state('statetwo',{
+        url: '/statetwo',
+        template: '<h1>State two</h1> <md-button ui-sref="two.more">Go to nested State</md-button><ui-view></ui-view>'
+    })
+    .state('two.more',{
+        url: '/more',
+        template: '<p>State two is deeper</p> '
+    })
 })
 .directive("helloWorld", function(){
 return{
