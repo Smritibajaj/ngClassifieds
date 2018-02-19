da75b0fb847c1ab0bc09a22462a46bf503377ed2
Smritibajaj <simmybajaj1603@gmail.com>
2018-02-19 15:41:32 +0530

    class 29 done

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,14 @@ angular
     .accentPalette('orange');
 
     $stateProvider
-    .state('stateone',{
+    .state('one',{
         url: '/stateone',
-        template: '<h1>State one</h1>'
+        template: '<h1>{{ message }}</h1>',
+        controller: 'stateOneCtrl as stateone'// DEFINE ALIAS
     })
-    .state('statetwo',{
+    .state('two',{
         url: '/statetwo',
-        template: '<h1>State two</h1> <md-button ui-sref="two.more">Go to nested State</md-button><ui-view></ui-view>'
+        template: '<h1>State two</h1> <md-button ui-sref="two.more">Go to nested State</md-button> <ui-view></ui-view>'
     })
     .state('two.more',{
         url: '/more',
@@ -24,4 +25,7 @@ angular
 return{
     template: "<h1> {{message}}</h1>"
 }
-});
+})
+.controller('stateOneCtrl', function(){
+$scope.message="Hey from state one";
+})
